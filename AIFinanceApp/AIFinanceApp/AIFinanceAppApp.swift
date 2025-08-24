be06b60d9c2a7f523cd2115be36bac3367511987
Santiago Prieto <santiprieto199@hotmail.com>
2025-08-24 02:51:50 -0600

    app files uploaded

---
+++ AIFinanceApp/AIFinanceApp/AIFinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIFinanceAppApp.swift
+//  AIFinanceApp
+//
+//  Created by Santiago Prieto on 24/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AIFinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
